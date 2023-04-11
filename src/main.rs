3cb7879829bb1d1bd6ab60c6d46db9ab397e770b
kawaemon <me@kawaemon.dev>
2023-04-11 12:50:48 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,87 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn main() {
-    let mut hex_file = BufReader::new(File::open("./out.hex").unwrap());
+struct HexDecoder<R> {
+    reader: R,
+}
 
-    let mut decoded = vec![];
-
-    for line in hex_file.lines() {
-        let line = line.unwrap().chars().collect::<Vec<_>>();
-        assert_eq!(line[0], ':');
-
-        let byte_count = decode_hex_u8((line[1], line[2])).unwrap();
-        let address = decode_hex_u16((line[3], line[4], line[5], line[6])).unwrap();
-
-        let record_type = decode_hex_u8((line[7], line[8])).unwrap();
-
-        let mut i = 9;
-        let mut next = || {
-            let v = i;
-            i += 1;
-            v
-        };
-        match record_type {
-            // data record
-            0 => {
-                println!("addr=0x{address:x}, bytes={byte_count}");
-                for i in 0..byte_count {
-                    let b = decode_hex_u8((line[next() as usize], line[next() as usize])).unwrap();
-                    if decoded.len() <= (address + i as u16) as usize {
-                        let needs = (address + i as u16) as usize - decoded.len() + 1;
-                        for _ in 0..needs {
-                            decoded.push(0);
-                        }
-                    }
-                    decoded[(address + i as u16) as usize] = b;
-                }
-            }
-            // EOF
-            1 => {
-                break;
-            }
-            _ => unimplemented!(),
-        }
+impl<R: BufRead> HexDecoder<R> {
+    pub fn new(reader: R) -> Self {
+        Self { reader }
     }
+
+    fn decode_hex_char(&mut self) -> Option<u8> {
+        let mut buf = [0; 1];
+        self.reader.read_exact(&mut buf).ok()?;
+
+        "0123456789ABCDEF"
+            .chars()
+            .position(|x| x as u8 == buf[0])
+            .map(|p| p as u8)
+    }
+
+    fn decode_hex_u8(&mut self) -> Option<u8> {
+        let c0 = self.decode_hex_char()?;
+        let c1 = self.decode_hex_char()?;
+        Some(c0 << 4 | c1) // Big-Endian
+    }
+
+    fn decode_hex_u16(&mut self) -> Option<u16> {
+        let c0 = self.decode_hex_u8()? as u16;
+        let c1 = self.decode_hex_u8()? as u16;
+        Some(c0 << 8 | c1) // Big-Endian
+    }
+
+    pub fn decode(mut self) -> Option<Vec<u8>> {
+        let mut decoded = vec![];
+
+        loop {
+            // TODO: decode newline, error handling
+            let mut buf = [0; 1];
+            self.reader.read_exact(&mut buf).unwrap();
+
+            assert_eq!(buf[0], ':' as u8);
+
+            let byte_count = self.decode_hex_u8()?;
+            let address = self.decode_hex_u16()? as u32; // for extended linear address command
+
+            let record_type = self.decode_hex_u8()?;
+
+            match record_type {
+                // data record
+                0 => {
+                    println!("addr=0x{address:x}, bytes={byte_count}");
+                    for i in 0..byte_count {
+                        let b = self.decode_hex_u8()?;
+                        let pos = (address + i as u32) as usize;
+
+                        if decoded.len() <= pos {
+                            let needs = pos - decoded.len() + 1;
+                            for _ in 0..needs {
+                                decoded.push(0);
+                            }
+                        }
+                        decoded[pos as usize] = b;
+                    }
+                }
+
+                // EOF
+                1 => break,
+
+                2..=5 => unimplemented!(),
+
+                _ => panic!("unknown record type"),
+            }
+        }
+
+        Some(decoded)
+    }
+}
+
+fn main() {
+    let decoded = HexDecoder::new(BufReader::new(File::open("./out.hex").unwrap()))
+        .decode()
+        .unwrap();
 
     for (i, &b) in decoded.iter().enumerate() {
         if i % 32 == 0 {
@@ -54,23 +93,4 @@ fn main() {
         print!("{b:02x} ");
     }
     println!();
-}
-
-fn decode_hex_char(c: char) -> Option<u8> {
-    "0123456789ABCDEF"
-        .chars()
-        .position(|x| x == c)
-        .map(|x| x as u8)
-}
-
-fn decode_hex_u8(c: (char, char)) -> Option<u8> {
-    let c0 = decode_hex_char(c.0)?;
-    let c1 = decode_hex_char(c.1)?;
-    Some(c0 << 4 | c1) // Big-Endian
-}
-
-fn decode_hex_u16(c: (char, char, char, char)) -> Option<u16> {
-    let c0 = decode_hex_u8((c.0, c.1))? as u16;
-    let c1 = decode_hex_u8((c.2, c.3))? as u16;
-    Some(c0 << 8 | c1) // Big-Endian
 }
