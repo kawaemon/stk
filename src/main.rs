418fa905c1c4dc6280a136c1d768cbda2339b661
kawaemon <me@kawaemon.dev>
2023-04-12 16:25:40 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,28 @@ fn main() {
         print!("{b:02x} ");
     }
     println!();
+
+    assert_eq!(decoded.len() % 2, 0);
+
+    for instruction in decoded.chunks(2) {
+        let &[a, b] = instruction else {
+            unreachable!()
+        };
+
+        let instruction = ((b as u16) << 8) | (a as u16);
+        println!("{instruction:016b}");
+
+        let masked = instruction & 0b0011_1111_1001_1111;
+        let is_noop = masked == 0;
+        if is_noop {
+            println!("noop")
+        }
+
+        let masked = instruction & 0b0011_1000_0000_0000;
+        let is_goto = (masked ^ 0b0010_1000_0000_0000) == 0;
+        if is_goto {
+            let k = instruction & 0b0000_0111_1111_1111;
+            println!("goto: k = {k:016b}");
+        }
+    }
 }
